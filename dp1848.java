a1123435ca0a22722f6fa90a17b3fc728e91b2cd
KimPyeongyeol(김평열) <yeolpyeong@gmail.com>
2017-09-12 14:18:22 +0900

    극장 좌석

--- dp1848.java
+++ dp1848.java
@@ -1,3 +1,8 @@
+/*
+ * 극장 좌석
+ * http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1121&sca=3050 
+ */
+
 package dynamic_programming1;
 
 import java.util.Scanner;
@@ -7,30 +12,30 @@ public class dp1848 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 		int res = 1;
-		int[] s = new int[m + 2];
+		int[] s = new int[N + 2];
 		s[0] = 0;
-		for (int i = 1; i <= m; i++) {
+		for (int i = 1; i <= M; i++) {
 			s[i] = sc.nextInt();
 		}
-		s[m + 1] = n + 1;
-		int[] t = new int[m + 1];
-		for (int i = 0; i < m + 1; i++) {
+		s[M + 1] = N + 1;
+		int[] t = new int[M + 1];
+		for (int i = 0; i < M + 1; i++) {
 			t[i] = s[i + 1] - s[i];
 			res *= fivonacci(t[i]);
 		}
 		System.out.println(res);
 	}
 
-	public static int fivonacci(int n) {
-		if (d[n] != 0)
-			return d[n];
+	public static int fivonacci(int N) {
+		if (d[N] != 0)
+			return d[N];
 
-		if (n == 1 || n == 2)
+		if (N == 1 || N == 2)
 			return 1;
 		else
-			return d[n] = fivonacci(n - 2) + fivonacci(n - 1);
+			return d[N] = fivonacci(N - 2) + fivonacci(N - 1);
 	}
 }
