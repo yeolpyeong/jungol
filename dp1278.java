0a5f78fd6751f745f3d305e7a2ec7698b74b1164
KimPyeongyeol(김평열) <yeolpyeong@gmail.com>
2017-09-12 14:13:37 +0900

    배낭 채우기2

--- dp1278.java
+++ dp1278.java
@@ -1,37 +1,42 @@
+/*
+ * 배낭 채우기2
+ * http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=561&sca=3050
+ */
+
 package dynamic_programming1;
 
 import java.util.Scanner;
 
 public class dp1278 {
-	static int n, w;
+	static int N, W;
 	static int[][] d = new int[10001][1001];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		w = sc.nextInt();
-		int[] wi = new int[n + 1];
-		int[] pi = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			wi[i] = sc.nextInt();
-			pi[i] = sc.nextInt();
+		N = sc.nextInt();
+		W = sc.nextInt();
+		int[] Wi = new int[N + 1];
+		int[] Pi = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			Wi[i] = sc.nextInt();
+			Pi[i] = sc.nextInt();
 		}
-		System.out.println(dp1278(wi, pi));
+		System.out.println(dp1278(Wi, Pi));
 	}
 
-	public static int dp1278(int[] wi, int[] pi) {
-		for (int i = 1; i <= w; i++) {
-			for (int j = 1; j <= n; j++) {
+	public static int dp1278(int[] Wi, int[] Pi) {
+		for (int i = 1; i <= W; i++) {
+			for (int j = 1; j <= N; j++) {
 				d[i][j] = d[i][j - 1];
 				// j번째 보석의 무게로 배낭을 채울 수 없는 경우 이전 보석으로 대체한다.
-				if (i - wi[j] >= 0 && d[i][j] < d[i - wi[j]][j - 1] + pi[j]) {
+				if (i - Wi[j] >= 0 && d[i][j] < d[i - Wi[j]][j - 1] + Pi[j]) {
 					// j번째 보석을 배낭의 남은 용량에 채울 수 있어야 한다.
 					// j번째 보석을 넣었을 때의 가치가 최대가 되어야 한다.
-					d[i][j] = d[i - wi[j]][j - 1] + pi[j];
+					d[i][j] = d[i - Wi[j]][j - 1] + Pi[j];
 				}
 				// System.out.println("(" + i + ", " + j + "): " + d[i][j]);
 			}
 		}
-		return d[w][n];
+		return d[W][N];
 	}
 }
